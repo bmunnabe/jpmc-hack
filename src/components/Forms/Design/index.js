3a78b5723f80b11763066105c66375dd9150d2df
bmunnabe <35695944+bmunnabe@users.noreply.github.com>
2023-06-16 01:41:27 +0530

    4gg4
    g4g4

---
+++ src/components/Forms/Design/index.js
@@ -0,0 +1,232 @@
+import React from 'react'
+
+export default function Design() {
+  return (
+    <div>
+        <section className="container">
+
+<header>
+    <div className="priority-col">
+        <label>Task </label>
+    </div>
+    <div className="status-col">
+        <label>Status </label>
+    </div>
+    <div className="progress-col">
+        <label>Progress </label>
+    </div>
+    <div className="dates-col">
+        <label>Dates </label>
+    </div>
+    <div className="icon-col">
+        <label>Action </label>
+    </div>
+    <div className="ref-col">
+        <label>References </label>
+    </div>
+</header>
+
+
+<ul className="task-items">
+    <li className="item type1">
+        <div className="task">
+            <div>Identify the gaps in the Architecture proposal + Document the gaps</div>
+        </div>
+        <div className="status">
+            <div className="icon off"> </div>
+            <div className="text"> Off Track </div>
+        </div>
+        <div className="progress">
+            <progress value="15" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user" >
+            <img src="https://source.unsplash.com/40x40/?coding" alt="Image 001" className="owner-img" />
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type4">
+    <div className="task">
+            <div>Present Experience Service Design - Block + Sequence and get the approval from architect</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="45" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?book" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type2">
+        <div className="task">
+            <div>Get/Validate Swagger Documentation from Downstream and analysis</div>
+        </div>
+        <div className="status">
+            <div className="icon off"> </div>
+            <div className="text"> Off Track </div>
+        </div>
+        <div className="progress">
+            <progress value="10" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type1">
+        <div className="task">
+            <div>Create Swagger Documentation for your own Tradker Experience Service (NDA Data Dictionary)</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>		
+    
+    <li className="item type1">
+        <div className="task">
+            <div>Addition of element(s) in CCB Dictionary (If Any)</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>	
+
+    <li className="item type1">
+        <div className="task">
+            <div>Swagger Approval by API Governance + Publish the API in API Store</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>	
+    <li className="item type1">
+        <div className="task">
+            <div>Sub user checks/FA/IP Analysis and implementation (If Any)</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>	
+    <li className="item type1">
+        <div className="task">
+            <div>Entitlements validations Analysis (If Any)</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>	
+    <li className="item type1">
+        <div className="task">
+            <div>Create a detailed design document having the following template - (Provide the template link here)</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>	
+</ul>
+
+</section>
+    </div>
+  )
+}
---
+++ src/components/Forms/Test/index.js
@@ -0,0 +1,132 @@
+
+import React from 'react'
+
+export default function Test() {
+  return (
+    <div>
+        <section className="container">
+
+<header>
+    <div className="priority-col">
+        <label>Task </label>
+    </div>
+    <div className="status-col">
+        <label>Status </label>
+    </div>
+    <div className="progress-col">
+        <label>Progress </label>
+    </div>
+    <div className="dates-col">
+        <label>Dates </label>
+    </div>
+    <div className="icon-col">
+        <label>Action </label>
+    </div>
+    <div className="ref-col">
+        <label>References </label>
+    </div>
+</header>
+
+
+<ul className="task-items">
+    <li className="item type1">
+        <div className="task">
+            <div>Go through Requirements/Documents/Confluence to understand bisuness requirement </div>
+        </div>
+        <div className="status">
+            <div className="icon off"> </div>
+            <div className="text"> Off Track </div>
+        </div>
+        <div className="progress">
+            <progress value="15" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user" >
+            <img src="https://source.unsplash.com/40x40/?coding" alt="Image 001" className="owner-img" />
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type4">
+    <div className="task">
+            <div>JAD with Product Managers for Business requirement</div>
+        </div>
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+        <div className="progress">
+            <progress value="45" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?book" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type2">
+        <div className="task">
+            <div>Go through Architecture Design given by Architect</div>
+        </div>
+        <div className="status">
+            <div className="icon off"> </div>
+            <div className="text"> Off Track </div>
+        </div>
+        <div className="progress">
+            <progress value="10" max="100" />
+        </div>
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?video" alt="Image 001" className="owner-img" />
+        </div>
+    </li>
+    
+    <li className="item type1">
+        <div className="task">
+            <div>Have meetings with Architect for any clarifications</div>
+        </div>
+
+        <div className="status">
+            <div className="icon warning"> </div>
+            <div className="text"> At Risk </div>
+        </div>
+
+        <div className="progress">
+            <progress value="35" max="100" />
+        </div>
+
+        <div className="dates">
+            <div className="bar"> </div>
+        </div>
+
+        <div className="priority">
+            <div className="bar"> </div>
+        </div>
+
+        <div className="user">
+            <img src="https://source.unsplash.com/40x40/?american" alt="Image 001" className="owner-img" />
+        </div>
+    </li>		
+    
+</ul>
+
+</section>
+    </div>
+  )
+}
